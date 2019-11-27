9db3ca0f9ecaee5e51970b5d7415c7059853d9e8
JJ Howard <jjhoward@blackboundent.com>
2019-11-26 20:08:28 -0600

    user signup 1

--- main.py
+++ main.py
@@ -1,10 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
-    return "Hello World"
+
+user_form = """
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            .error {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+    <h1>User Signup</h1>
+        <form method="post">
+            <table>
+                <tr>
+                    <td><label for="username">Username</label></td>
+                    <td>
+                        <input name="username" type="text" value="">
+                        <span class="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="password">Password</label></td>
+                    <td>
+                        <input name="password" type="password">
+                        <span class="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="verify">Verify Password</label></td>
+                    <td>
+                        <input name="verify" type="password">
+                        <span class="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="email">Email (optional)</label></td>
+                    <td>
+                        <input name="email" value="">
+                        <span class="error"></span>
+                    </td>
+                </tr>
+            </table>
+            <input type="submit">
+        </form>
+    </body>
+</html>
+"""
+
+
+@app.route('/')
+def display_user_form():
+    return user_form
+
+
+
 
 app.run()
